3800e0fa475a229ac1fdcee757be65452d692696
Zheyuan-Fan <zheyuan94fan@gmail.com>
2025-05-03 13:56:19 +0800

    mortality.py

--- mortality.py
+++ mortality.py
@@ -62,6 +62,9 @@ Creatinine = st.sidebar.number_input("Creatinine:", min_value=38.00, max_value=1
 feature_values = [CPR_time, Initial_cardiac_rhythm, Bystander_CPR, SOFA_score, sTREM_1, NSE, IL_6, IL_10, CRP, hs_TnI, Creatinine]  # 收集所有输入的特征
 features = np.array([feature_values])  # 转换为NumPy数组
 
+#
+explainer = shap.TreeExplainer(model)
+
 if st.button("Make Prediction"):  # 如果点击了预测按钮
     # Predict the class and probabilities
     predicted_class = model.predict(features)[0]  # 预测类别
@@ -114,3 +117,16 @@ if st.button("Make Prediction"):  # 如果点击了预测按钮
 
     # Show the plot
     st.pyplot(plt)  # 显示图表
+
+    ## SHAP力图生成 
+    shap_values = explainer(features)
+
+    # 使用Matplotlib渲染 
+    plt.figure(figsize=(10,6)) 
+    shap.plots.waterfall(shap_values[0],  max_display=10)
+    st.pyplot(plt.gcf()) 
+    
+    # 交互式HTML展示 
+    st.markdown("Prediction Model with SHAP Visualization")
+    shap_html = shap.plots.force(shap_values[0],  matplotlib=False)
+    st.components.v1.html(shap_html,  height=400)
